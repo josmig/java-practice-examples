bea30ee2bd823b6a9a7c22c9036b97ad8eae2a33
Miguel Pantigoso <pantigosomiguel59@gmail.com>
2020-04-18 23:28:07 -0500

    init practice

---
+++ src/2-Variables/main.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main(String []args){
+        var hola ="hoamundo";
+        var h = hola.equals("asdaas") ? "nada" : "mundo";
+        System.out.println(h);
+    }
+}
